70f3bcf541e826fe4e430ae81aa976e330c3b208
FlashScript <itzflashscript@gmail.com>
2025-01-04 10:17:50 +0330

    Insert New Data to a Table

--- src/test.sql
+++ src/test.sql
@@ -5,11 +5,27 @@
 
 CREATE TABLE PopularGames (
     name VARCHAR(50),
-    ratings DECIMAL,
-    year INT
+    release_year INT,
+    ratings DECIMAL
 )
 
+-- DROP Table PopularGames;
 -- SHOW TABLES;
+-- SHOW COLUMNS FROM PopularGames;
+-- DESCRIBE PopularGames;
 
-SHOW COLUMNS FROM PopularGames;
-DESCRIBE PopularGames;
+-- INSERT INTO PopularGames (name, release_year, ratings)
+-- VALUES ("GTA 6", 2025, 9.7);
+
+INSERT INTO PopularGames (name, release_year, ratings)
+VALUES ("Resident Evil 4 Remake", 2023, 8.7),
+       ("Apex Legends", 2019, 7.6),
+       ("Silent Hill 2 Remake", 2024, 8.3),
+       ("Cyberpunk 2077", 2020, 7.2);
+
+-- SELECT * FROM PopularGames;
+
+SELECT name FROM PopularGames;
+SELECT release_year FROM PopularGames;
+SELECT ratings FROM PopularGames;
+SELECT name, release_year, ratings FROM PopularGames;
