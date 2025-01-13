ea8f619b7ed466ec170179daadf9a9091e6bf7f7
FlashScript <itzflashscript@gmail.com>
2025-01-13 18:29:36 +0330

    Advanced Default Values
    using "ALTER" to add a default value to an existing Table

--- src/recap.sql
+++ src/recap.sql
@@ -1,14 +1,10 @@
--- ALTER DATABASE mydb READ ONLY = 0; 
+-- if you forgot to add "DEFAULT" keyword to a table,
+-- add it using "ALTER"
+CREATE TABLE users (
+	user_id INT,
+    name VARCHAR(50),
+    age INT
+);
 
--- CREATE TABLE employees (
--- 	employee_id INT,
---     first_name VARCHAR(50),
---     last_name VARCHAR(50),
---     hourly_pay DECIMAL(3, 2), -- 123.12
---     hire_date DATE
--- );
-
--- SELECT * FROM employees;
-
--- Let's rename our "employees" table
-RENAME TABLE workers TO employees;
+ALTER TABLE users
+ALTER name SET DEFAULT "unknown";
