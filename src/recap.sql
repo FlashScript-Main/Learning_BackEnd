8390cee76a7c1372049ec7bc924832f0cd84e6d8
FlashScript <itzflashscript@gmail.com>
2025-01-15 14:38:41 +0330

    Advance FUNCTIONS in MySQL

--- src/recap.sql
+++ src/recap.sql
@@ -1,22 +1,27 @@
--- INNER JOIN
-SELECT * 
-FROM transactions INNER JOIN customers
-ON transactions.customer_id = customers.customer_id;
+-- SELECT * FROM transactions;
+
+SELECT COUNT(amount) AS "today's transactions"
+FROM transactions;
+-- T -> 5
+
+SELECT MAX(amount) AS maximum_value 
+FROM transactions;
+-- T -> 4.99
+
+SELECT MIN(amount) AS minimum_value 
+FROM transactions;
+-- T -> 1.00
+
+SELECT AVG(amount) AS average_value 
+FROM transactions;
+-- T -> 3.450000
+
+SELECT SUM(amount) AS sum 
+FROM transactions;
+-- T -> 17.25
 
 
--- You can Select Specific Columns for any joins
-SELECT transaction_id, amount, first_name, last_name
-FROM transactions INNER JOIN customers 
-ON transactions.customer_id = customers.customer_id;
+-- SELECT * FROM employees
 
-
--- LEFT JOIN
-SELECT *
-FROM transactions LEFT JOIN customers 
-ON transactions.customer_id = customers.customer_id;
-
-
--- RIGHT JOIN
-SELECT *
-FROM transactions RIGHT JOIN customers 
-ON transactions.customer_id = customers.customer_id;
+SELECT CONCAT(first_name, " ", last_name) AS full_name
+FROM employees;
