c243e342a60cedef8a22021f3308960c43e2fae5
FlashScript <itzflashscript@gmail.com>
2025-01-16 11:26:12 +0330

    Advance SUBQUERIES & GROUP BY in MySQL

--- src/recap.sql
+++ src/recap.sql
@@ -1,27 +1,68 @@
--- SELECT * FROM transactions;
+DROP TABLE IF EXISTS transactions;
 
-SELECT COUNT(amount) AS "today's transactions"
-FROM transactions;
--- T -> 5
+CREATE TABLE transactions (
+    transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+    amount DECIMAL(5, 2),
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) 
+    REFERENCES customers(customer_id)
+);
 
-SELECT MAX(amount) AS maximum_value 
-FROM transactions;
--- T -> 4.99
+INSERT INTO transactions
+VALUES (1000, 4.99, 3, "2023-01-01"),
+       (1001, 2.89, 2, "2023-01-01"),
+	   (1002, 3.38, 3, "2023-01-02"),
+	   (1003, 4.99, 1, "2023-01-02"),
+       (1004, 1.00, NULL, "2023-01-03"),
+       (1005, 2.49, 4, "2023-01-03"),
+       (1006, 5.48, NULL, "2023-01-03");
+        
+SELECT * FROM transactions;
 
-SELECT MIN(amount) AS minimum_value 
-FROM transactions;
--- T -> 1.00
+-- now our boss Mr.Krabs he needs us to tell him how much money he made per day
+-- what is the sum of all of the amounts per day 
+-- we have three different dates, January 1st January 2nd January 3rd 
+-- what is the sum of every amount on these days 
+-- so we can use the group I Clause that would help us
 
-SELECT AVG(amount) AS average_value 
-FROM transactions;
--- T -> 3.450000
+SELECT SUM(amount), order_date
+FROM transactions
+GROUP BY order_date;
 
-SELECT SUM(amount) AS sum 
-FROM transactions;
--- T -> 17.25
+-- let's Group by "customer_id" this time 
+-- how much has each customer spent total?
 
+SELECT SUM(amount), customer_id
+FROM transactions
+GROUP BY customer_id;
 
--- SELECT * FROM employees
+SELECT COUNT(amount), customer_id
+FROM transactions
+GROUP BY customer_id;
 
-SELECT CONCAT(first_name, " ", last_name) AS full_name
-FROM employees;
+SELECT AVG(amount), customer_id
+FROM transactions
+GROUP BY customer_id;
+
+SELECT MAX(amount), customer_id
+FROM transactions
+GROUP BY customer_id;
+
+SELECT MIN(amount), customer_id
+FROM transactions
+GROUP BY customer_id;
+
+-- what if we want to use "WHERE" clause?
+-- using a "WHERE" clause along with the "GROUP BY" clause,
+-- normally does NOT work! for example
+-- I would like to display all customers that have visited more than once
+-- who are the repeat customers?
+
+-- if you want to use "WHERE" clause with "GROUP BY" clause,
+-- instead you need to use the word "HAVING"
+
+SELECT COUNT(amount), customer_id
+FROM transactions
+GROUP BY customer_id
+HAVING COUNT(amount) > 1 AND customer_id is NOT NULL;
