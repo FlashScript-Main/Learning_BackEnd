7aabcaddd9d1e65bad6872dd98ade0c9f58fcca0
FlashScript <itzflashscript@gmail.com>
2025-01-17 20:55:02 +0330

    Advance Triggers in MySQL

--- src/recap.sql
+++ src/recap.sql
@@ -1,80 +1,139 @@
--- 🔻 Stored Procedure 🔻
--- it is prepared SQL code that you can save
--- great if there's a query that you write often
+-- 🔻 Trigger 🔻
+-- when an event happens, do something
+-- examples: INSERT, UPDATE, DELETE
+-- checks data, handles errors, auditing tables
 
--- you can use "Stored Procedure" to store a code and call it later
-
-
--- 🔥 Basic Format 🔥
-
--- #1 👉🏻 Create the "Stored Procedure"
-DELIMITER $$
-CREATE PROCEDURE The_Procedure_Name()
-BEGIN
-    The_Procedure_Phrase;
-END $$
-DELIMITER ;
-
--- #2 👉🏻 "CALL" the "Stored Procedure"
-CALL The_Procedure_Name();
-
--- 🔹 Before 🔹
-SELECT * FROM customers;
-
--- 🔹 After 🔹
-DELIMITER $$
-CREATE PROCEDURE get_customers()
-BEGIN
-	SELECT * FROM customers;
-END $$
-DELIMITER ;
-
-CALL get_customers();
+SELECT * FROM employees;
 
 
--- you can also Drop a Stored Procedure
-DROP PROCEDURE get_customers;
+ALTER TABLE employees
+ADD COLUMN salary DECIMAL(10, 2) AFTER hourly_pay;
+
+SELECT * FROM employees;
 
 
--- let's try another example
--- this time we will send our procedure a piece of data 
--- within the set of parentheses 
--- a "customer_id" number such as 1 2 3 4 so on and so forth
--- we will get a customer by their "customer_id"
-DELIMITER $$
-CREATE PROCEDURE find_customer(IN id INT)
-BEGIN
-	SELECT * FROM customers
-    WHERE customer_id = id;
-END $$
-DELIMITER ;
+-- Let's update our employees table base on Salary year payment
+-- 400 hours per week
+-- 52 weeks per year
+UPDATE employees
+SET salary = hourly_pay * 400 * 52;
+
+SELECT * FROM employees;
 
 
--- 🔹 Before 🔹
-SELECT * FROM customers
-WHERE customer_id = 3;
-
--- 🔹 After 🔹
-CALL find_customer(3);
-
-
--- this time we will send two arguments, two pieces of data 
--- a first name and a last name
-DELIMITER $$
-CREATE PROCEDURE find_customer_by_name(
-				     IN first_customer_name VARCHAR(50), 
-                     IN last_customer_name VARCHAR(50)
-				    )
-BEGIN
-	SELECT * FROM customers
-    WHERE first_name = first_customer_name AND last_name = last_customer_name;
-END $$
-DELIMITER ;
+-- what i want is whenever we update an employee's hourly pay, 
+-- I would like to also update the salary automatically with the trigger 
+-- I don't want to have to calculate every employee's salary manually, using a calculator 
+CREATE TRIGGER before_hourly_pay_update
+BEFORE UPDATE ON employees
+FOR EACH ROW
+SET NEW.salary = (NEW.hourly_pay * 400 * 52);
 
 
--- 🔹 Before 🔹
-SELECT * FROM customers
-WHERE first_name = "Larry" AND last_name = "Lobster";
+-- See your Triggers
+SHOW TRIGGERS;
 
--- 🔹 After 🔹
-CALL find_customer_by_name("Larry", "Lobster");
+
+UPDATE employees
+SET hourly_pay = 50
+WHERE employee_id = 1;
+
+SELECT * FROM employees;
+
+
+-- let's create a new trigger 
+-- we will calculate a salary whenever we insert a new employee 
+-- more specifically whenever we insert a new hourly pay 
+-- right now we have a trigger that only kicks in when we update an hourly pay
+CREATE TRIGGER before_hourly_pay_insert
+BEFORE INSERT ON employees
+FOR EACH ROW
+SET NEW.salary = (NEW.hourly_pay * 400 * 52);
+
+
+-- Let's insert a new employee
+INSERT INTO employees
+VALUES(6, "Sheldon", "Plankton", 10, NULL);
+
+SELECT * FROM employees;
+
+
+-- I do have a few more examples 
+-- triggers are something you'll want to master
+-- they're really helpful 
+-- we're going to create a new table this time
+CREATE TABLE expenses(
+	expense_id INT PRIMARY KEY AUTO_INCREMENT,
+    expense_name VARCHAR(50),
+    expense_total DECIMAL(10, 2)
+);
+
+INSERT INTO expenses (expense_name, expense_total)
+VALUES ("salaries", 0),
+       ("supplies", 0),
+       ("taxes", 0);
+       
+SELECT * FROM expenses;
+
+
+-- Let's calculate the expense total of our salaries
+UPDATE expenses
+SET expense_total = (SELECT SUM(salary) FROM employees)
+WHERE expense_name = "salaries";
+
+SELECT * FROM expenses;
+
+
+-- whenever we delete an employee, 
+-- we will update this value found within another table our expenses table
+-- so let's create a trigger for it
+CREATE TRIGGER after_salary_delete
+AFTER DELETE ON employees
+FOR EACH ROW
+UPDATE expenses
+SET expense_total = expense_total - OLD.salary
+WHERE expense_name = "salaries";
+
+
+-- I'm going to delete Plankton, he's fired!
+DELETE FROM employees
+WHERE employee_id = 6; 
+
+SELECT * FROM expenses;
+
+
+-- Let's create a trigger that will update our salaries 
+-- whenever we insert a new employee
+CREATE TRIGGER after_salary_insert
+AFTER INSERT ON employees
+FOR EACH ROW
+UPDATE expenses
+SET expense_total = expense_total + NEW.salary
+WHERE expense_name = "salaries";
+
+
+-- Let's Insert a new employee
+INSERT INTO employees
+VALUES(6, "Sheldon", "Plankton", 10, NULL);
+
+SELECT * FROM expenses;
+
+
+-- one last example
+-- Let's create a trigger that will update "expense_total" value,
+-- when we change an employee salary 
+-- and that will probably be the most complicated one
+CREATE TRIGGER after_salary_update
+AFTER UPDATE ON employees
+FOR EACH ROW
+UPDATE expenses
+SET expense_total = expense_total + (NEW.salary - OLD.salary)
+WHERE expense_name = "salaries";
+
+
+-- Mr.Krabs is going to give himself another pay raise
+UPDATE employees
+SET hourly_pay = 100
+WHERE employee_id = 1;
+
+SELECT * FROM expenses;
