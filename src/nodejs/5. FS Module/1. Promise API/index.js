98b1cf1a928d54ddab87e2974e6bb2e2251c54fe
FlashScript <itzflashscript@gmail.com>
2025-01-25 19:49:08 +0330

    Advanced FS Imports in Node.js

--- src/nodejs/5. FS Module/1. Promise API/index.js
+++ src/nodejs/5. FS Module/1. Promise API/index.js
@@ -232,3 +232,48 @@ try {
 catch (error) {
     console.log(error)
 }
+
+// Advanced FS Imports in Node.js
+import * as fs from "fs";
+
+/*
+// Creating a Directory/Folder using "Callback API"
+fs.mkdir(
+    "c:/Users/Admin/Desktop/nodejs/5. FS Module/2. Callback API/test",
+    (error) => {
+        if (error) throw error;
+        console.log("Directory/Folder created!");
+    }
+);
+*/
+
+
+/*
+    🔹 so in this case like you can read the file, you can write to a file,
+    you can read the content from one file, you can do all of their stuff 
+    which I just mentioned inside This "Promise API" 
+    but in this case you don't have to "try catch" block 
+    and in this case you can specify callback function right here
+    you can either provide this function with the name 
+    or you can provide a callback function 🔹
+*/
+
+
+// Callback Function
+fs.mkdir(
+    "c:/Users/Admin/Desktop/nodejs/5. FS Module/2. Callback API/test",
+    (error) => {
+        if (error) throw error;
+        console.log("Directory/Folder created!");
+    }
+);
+
+
+// Callback Function with Name
+fs.mkdir(
+    "c:/Users/Admin/Desktop/nodejs/5. FS Module/2. Callback API/test",
+    function (error) {
+        if (error) throw error;
+        console.log("Directory/Folder created!");
+    }
+);
