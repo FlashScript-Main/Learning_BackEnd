283fac345c7a23234479ebb80e04d14fa3054e9b
FlashScript <itzflashscript@gmail.com>
2025-02-05 19:52:33 +0330

    Advance Multiple-Routes in Express.js

--- src/express.js/0. recap/Advance Routing/index.js
+++ src/express.js/0. recap/Advance Routing/index.js
@@ -1,32 +1,80 @@
+// Root 👉🏻 index.js
+
+
+/*  
+    🔸 Route Parameters 🔸
+
+    🔹 so "Route Parameters" are the name URL segments 
+    that are used to capture the value specified at their position in the URL 
+    so in some situation you want to capture some sort of data from the user 
+    like it doesn't matter if the user select, I don't know some sort of products 
+    like let's suppose if you are building some sort of an e-commerce application 🔹
+
+    🔻 Examples 🔻
+        🔰 e-commerce/products/iphone 🔰
+        🔰 e-commerce/products/tablet 🔰
+        🔰 e-commerce/products/laptop 🔰
+        🔰 e-commerce/products/watch 🔰
+        🔰 e-commerce/products/camera 🔰
+    
+        ⚡ e-commerce/products/:id ⚡
+*/
+
 import express from "express";
+
 
 const app = express();
 
-// app.get();
-// app.post();
-// app.put();
-// app.delete();
 
 app.get("/products/:name", (req, res) => {
+    // You can also destructure the route parameters
     const { name } = req.params;
-    res.send(`This is the product with name of ${name}`);
+
+    res.send(`Product Name: ${name}`);
 });
 
-app.get("/", (req, res) => {
-    res.send("Home Page");
+
+// You can also provide Multiple Route Parameters
+app.get("/products/:category/:id", (req, res) => {
+    const { category, id } = req.params;
+
+    res.send(`Product Category: ${category}, Product ID: ${id}`);
 });
 
-app.get("/about", (req, res) => {
-    res.send("About Page");
+app.get("/products/order/:day/:month/:year", (req, res) => {
+    const { day, month, year } = req.params;
+
+    res.send(`ProductOrder Date: ${day}/${month}/${year}`);
 });
 
-app.get("/contact", (req, res) => {
-    res.send("<h1>Contact Page</h1>");
+
+/*
+    🔹 Now Let's talk about "app.param()" method
+    it is used to add the Callback trigger to the raw parameters
+    and it is commonly used to check the existence of the data requested 
+    related to the same route param 🔹
+
+    ⚡ Basic Format ⚡
+    app.param(ParamName, Callback-Function);
+*/
+
+app.param("id", (req, res, next, id) => {
+    console.log(`id: ${id}`);
+
+    // You can also use the "isNaN()" method to check the existence of the data
+    if (isNaN(id)) {
+        return res.status(400).send("Invalid ID");
+    }
+
+    next();
 });
 
-app.get("*", (req, res) => {
-    res.send("404 Page");
+app.get("/products/:id", (req, res) => {
+    const { id } = req.params;
+
+    res.send(`Product ID: ${id}`);
 });
+
 
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
