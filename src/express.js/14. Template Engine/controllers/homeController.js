174a3102f12265f7aaf54823f70adb2b56baf8fd
FlashScript <itzflashscript@gmail.com>
2025-02-07 19:25:26 +0330

    Template Engine in Express.js (Setup)

---
+++ src/express.js/14. Template Engine/controllers/homeController.js
@@ -0,0 +1,9 @@
+// Root 👉🏻 "controllers" Folder 👉🏻 homeController.js
+
+
+const homeController = (req, res) => {
+    res.render("index"); // render the index.ejs file
+}
+
+
+export default homeController;
