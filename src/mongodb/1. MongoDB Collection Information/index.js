8048e09ecfe27f9184e4c06fd7e25973c3ce748d
FlashScript <itzflashscript@gmail.com>
2025-02-11 22:26:51 +0330

    MongoDB Initial Setup

---
+++ src/mongodb/1. MongoDB Collection Information/index.js
@@ -0,0 +1,25 @@
+/*
+    🔸 MongoDB Collection Information 🔸
+
+    1️⃣ 👉🏻 Open CMD 
+    2️⃣ 👉🏻 Type "mongosh"
+    3️⃣ 👉🏻 Type "show dbs"
+    4️⃣ 👉🏻 Type "db.getName()"
+    5️⃣ 👉🏻 JavaScript in MongoDB
+    6️⃣ 👉🏻 JSON in MongoDB
+*/
+
+
+/*
+    🔸 show.dbs 🔸
+    🔹 allows us to show or to check all of our databases 🔹
+
+    🔸 db.getName() 🔸
+    🔹 allows us to get the name of the database we are currently in 🔹
+
+    🔸 db.Collection-Name.insertOne() 🔸
+    🔹 Insert a new Collection to our database 🔹
+
+    🔸 show collections 🔸
+    🔹 allows us to show all of our collections 🔹
+*/
---
+++ src/mongodb/1. MongoDB Collection Information/public/app.js
@@ -0,0 +1,7 @@
+// Root 👉🏻 "public" Folder 👉🏻 app.js
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    alert("MERN Stack");
+});
