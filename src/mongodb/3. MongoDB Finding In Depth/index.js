7f10347abbcc4811447e3619b54f66ca176d2995
FlashScript <itzflashscript@gmail.com>
2025-02-12 18:41:00 +0330

    MongoDB Finding In Depth

---
+++ src/mongodb/3. MongoDB Finding In Depth/index.js
@@ -0,0 +1,43 @@
+/*
+    🔸 MongoDB Collection Information 🔸
+
+    1️⃣ 👉🏻 Open CMD 
+    2️⃣ 👉🏻 Type "mongosh"
+    3️⃣ 👉🏻 Type "show dbs"
+    4️⃣ 👉🏻 Type "db.getName()"
+    5️⃣ 👉🏻 JavaScript in MongoDB
+    6️⃣ 👉🏻 JSON in MongoDB
+*/
+
+
+/*
+    🔸 show.dbs 🔸
+    🔹 allows us to show or to check all of our databases 🔹
+
+    🔸 db.getName() 🔸
+    🔹 allows us to get the name of the database we are currently in 🔹
+
+    🔸 db.Collection-Name.insertOne() 🔸
+    🔹 Insert a new Collection to our database 🔹
+
+    🔸 show collections 🔸
+    🔹 allows us to show all of our collections 🔹
+*/
+
+
+/*
+    🔸 db.Collection-Name.insertOne() 🔸
+    🔹 Insert a new Collection to our database 🔹
+
+    🔸 db.Collection-Name.insertMany() 🔸
+    🔹 Insert multiple new Collections to our database 🔹
+
+    🔸 db.Collection-Name.find() 🔸
+    🔹 Find a Collection in our database 🔹
+
+    🔸 db.Collection-Name.findOne() 🔸
+    🔹 Find a single Collection in our database 🔹
+
+    🔸 db.Collection-Name.findMany() 🔸
+    🔹 Find multiple Collections in our database 🔹
+*/
