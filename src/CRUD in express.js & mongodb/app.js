ac89fb3bbbaa809b7ffb89b95694e313efae280a
FlashScript <itzflashscript@gmail.com>
2025-02-17 10:41:59 +0330

    API Connection between Back-End & Database

--- src/CRUD in express.js & mongodb/app.js
+++ src/CRUD in express.js & mongodb/app.js
@@ -1,37 +1,19 @@
-// Mongo DB CRUD Demo
+const express = require('express')
+const app = express()
+const port = 8000
+const routeHandler = require('./routes')
+const router = routeHandler()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const logger = require('morgan')
+const helmet = require('helmet')
 
-// Modules
-var mongoose = require('mongoose')
-var express = require('express')
-var fs = require('fs')
+app.use(bodyParser.urlencoded({ extended: false }))
+  .use(logger('dev'))
+  .use(helmet())
+  .use(cors())
+  .use(bodyParser.json())
 
-// express setup
-var app = express()
-var port = process.env.PORT || 3000
+app.use(router)
 
-// Controllers
-var personController = require('./controllers/personController')
-var personSetup = require('./controllers/setupController')
-
-// load mongo config
-var mongoConfig = JSON.parse(fs.readFileSync(__dirname + '/config/mongo-config.json', 'utf8'));
-
-var mongourl = mongoConfig.mongourl
-
-// configure assets and vies
-app.use('/assets', express.static(__dirname + '/public'))
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs')
-
-
-// connect to mongodb
-mongoose.connect(mongourl)
-
-// init controllers
-personController(app, mongoose)
-personSetup(app, mongoose)
-
-// kick web server off
-app.listen(port)
-
-console.log('mongo client listening on port', port)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
