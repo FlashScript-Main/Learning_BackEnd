e3bdaab83d33d5eeb992c3297c121ff3d5115043
FlashScript <itzflashscript@gmail.com>
2025-03-05 09:35:15 +0330

    Mongoose Setup & Schema Creation

---
+++ src/mongodb/10. Mongoose Schema and Model/index.js
@@ -0,0 +1,28 @@
+// Root 👉🏻 index.js
+
+
+import express from 'express';
+import connectDB from './db/connectDB.js';
+
+// Import Model
+import movieModel from './models/Movies.js'; // HuXn: do NOT forget the ".js" extension
+
+
+const app = express();
+
+
+const port = process.env.PORT || 8000;
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017";
+
+
+connectDB(`${DATABASE_URL}/movies`);
+
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+});
